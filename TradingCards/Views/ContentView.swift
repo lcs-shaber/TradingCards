8cc5b29b647c45a5da8ec356b4335a79efa6a9c9
Sophie Haber <SHaber@lcs.on.ca>
2024-01-21 16:50:17 -0500

    I copied and pasted the basic layout for the bottom over from a different project, which I had because my preview wasn't loading and so I used a different project so I could continue working.

--- TradingCards/Views/ContentView.swift
+++ TradingCards/Views/ContentView.swift
@@ -10,6 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
+            
             HStack{
                 Text("TENNIS")
                     .font(Font.system(size: 40))
@@ -21,10 +22,59 @@ struct ContentView: View {
                 PlayerName: "Novak Djokovic",
                 PlayerImage: "Djokovic"
             )
+            .padding(.horizontal)
+            
+            Spacer()
+            
+                ZStack{
+                    Rectangle()
+                        .foregroundStyle(.pink)
+                        .frame(height: 100)
+                    
+                    HStack{
+                        VStack{
+                            Text("Country:")
+                            Text("........")
+                        }
+                        Spacer()
+                        // tennis Logo
+                        
+                        VStack {
+                            Text("Ranking:")
+                            Text("........")
+                        }
+                        
+                        
+                    }
+                    .padding()
+                    
+                }
+                .padding(.vertical)
+                Spacer()
+            
+            HStack{
+                Text("Wins:")
+                Text(".....")
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Rectangle()
+                .fill(.gray)
+                .frame(height: 30)
+            
+                .padding(.bottom)
+            
+            Rectangle()
+                .fill(.purple)
+                .ignoresSafeArea()
+                .frame(height: 70)
+            
+        
+            
             
             
         }
-        .padding()
     }
 }
 
