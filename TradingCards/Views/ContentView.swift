68f7de01e01640d86df36740aea374a0de85c803
Sophie Haber <SHaber@lcs.on.ca>
2024-01-21 18:36:19 -0500

    I created a background. I just have to make my own colours so the colours are correct.

--- TradingCards/Views/ContentView.swift
+++ TradingCards/Views/ContentView.swift
@@ -9,44 +9,50 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            
-            HStack{
-                Text("TENNIS")
-                    .font(Font.system(size: 40))
-                Spacer()
-            }
-            .padding()
-            
-            ImageView(
-                PlayerName: "Novak Djokovic",
-                PlayerImage: "Djokovic"
-            )
-            .padding(.horizontal)
-            
-            Spacer()
-            
-            AboutThePlayerView(
-                PlayersCountry: "Serbia",
-                PlayersRanking: "world No. 1 in singles",
-                PlayersWins: "98 singles titles"
-            )
-            
-            Rectangle()
-                .fill(.gray)
-                .frame(height: 30)
-            
-                .padding(.bottom)
-            
-            Rectangle()
-                .fill(.purple)
-                .ignoresSafeArea()
-                .frame(height: 70)
-            
         
+        ZStack {
+            Color.yellow
+                .ignoresSafeArea()
             
-            
-            
+            VStack {
+                
+                HStack{
+                    Text("TENNIS")
+                        .font(Font.system(size: 40))
+                    Spacer()
+                }
+                .padding()
+                
+                ImageView(
+                    PlayerName: "Novak Djokovic",
+                    PlayerImage: "Djokovic"
+                )
+                .padding(.horizontal)
+                
+                Spacer()
+                
+                AboutThePlayerView(
+                    PlayersCountry: "Serbia",
+                    PlayersRanking: "world No. 1 in singles",
+                    PlayersWins: "98 singles titles"
+                )
+                
+                Rectangle()
+                    .fill(.gray)
+                    .frame(height: 30)
+                
+                    .padding(.bottom)
+                
+                Rectangle()
+                    .fill(.purple)
+                    .ignoresSafeArea()
+                    .frame(height: 70)
+                
+                
+                
+                
+                
+            }
         }
     }
 }
