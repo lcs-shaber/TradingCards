2cd23b9df2a0e7f6b3cabbfc3464c6da6549b6ee
Sophie Haber <SHaber@lcs.on.ca>
2024-01-22 12:27:28 -0500

    I added a title to my list

--- TradingCards/Views/PlayerListView.swift
+++ TradingCards/Views/PlayerListView.swift
@@ -10,20 +10,36 @@ import SwiftUI
 struct PlayerListView: View {
     var body: some View {
         
-        NavigationStack{
-            List{
+        VStack{
+            
+            HStack{
+                Text("Player Overview:")
+                    .font(.largeTitle)
+                    .bold()
+                    .foregroundColor(.darkPurple)
                 
-                //First Item
-                NavigationLink(destination: {
-                    ContentView()
-                },
-                               label: {
-                    ListDescription(
-                        PlayerProfilePicture: "Djokovic",
-                        PlayerProfileName: "Novac Djokovic"
-                    )
-                })
-                
+                Spacer()
+            }
+            .padding()
+            
+            NavigationStack{
+                List{
+                    
+                    //First Item
+                    NavigationLink(destination: {
+                        ContentView()
+                    },
+                                   label: {
+                        ListDescription(
+                            PlayerProfilePicture: "Djokovic",
+                            PlayerProfileName: "Novac Djokovic"
+                        )
+                    })
+                    
+                    //Second Item
+                    
+                    
+                }
             }
         }
         
